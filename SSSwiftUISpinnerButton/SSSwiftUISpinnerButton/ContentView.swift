51ca20aa6a7851bb8f4c300ed4b96efd395726a3
Nilisha gupta <nilisha.g@simformsolutions.com>
2021-03-04 21:31:10 +0530

    Setup project

---
+++ SSSwiftUISpinnerButton/SSSwiftUISpinnerButton/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SSSwiftUISpinnerButton
+//
+//  Created by Nilisha Gupta on 12/02/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
